e4dac05681a5e82a069821fe34828ff0dbffa784
Joehwang <joehwang.com@gmail.com>
2017-05-03 16:39:46 +0800

    init

---
+++ app/App.vue
@@ -0,0 +1,33 @@
+<template>
+<div id="app">
+<p>hello world</p>
+<p>1.open url</p>
+<p>2.clipboard writeText:"Example String"</p>
+<button class="ui button active" @click="exopen">electron lib test</button>
+</div>
+</template>
+
+<script>
+import { shell,clipboard } from 'electron'
+export default {
+    components: {
+    },
+    data () {
+      return {
+
+      }
+    },
+    filters: {
+
+    },
+    mounted () {
+
+    },
+    methods: {
+      exopen(){
+        shell.openExternal("http://joinwed.com")
+        clipboard.writeText('Example String')
+      }
+    }
+  }
+</script>
---
+++ app/main.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+new Vue({
+  el: document.getElementById("vue_instance"),
+  components: { App }
+})
